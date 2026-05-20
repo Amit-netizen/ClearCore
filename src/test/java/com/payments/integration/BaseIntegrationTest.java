c27f87ad4125456a5864d753cd1b05455dea74cd
amit-netizen <akakumar635@gmail.com>
2026-05-20 22:01:38 +0530

    fix: use KafkaContainer instead of ConfluentKafkaContainer for testcontainers 1.19.x

--- src/test/java/com/payments/integration/BaseIntegrationTest.java
+++ src/test/java/com/payments/integration/BaseIntegrationTest.java
@@ -3,11 +3,11 @@ package com.payments.integration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.kafka.ConfluentKafkaContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -27,9 +27,8 @@ public abstract class BaseIntegrationTest {
                     .withExposedPorts(6379);
 
     @Container
-    static final ConfluentKafkaContainer kafka =
-            new ConfluentKafkaContainer(
-                    DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
+    static final KafkaContainer kafka =
+            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.4.0"));
 
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry) {
